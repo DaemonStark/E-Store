551f75efca9f9c629bb4f8af00809b764a5e24d0
Rituja Shinde <65076102+ritujashinde97@users.noreply.github.com>
2020-06-02 18:11:11 +0530

    Updated cart total

--- store/views.py
+++ store/views.py
@@ -6,31 +6,51 @@ from.models import *
 
 # Create your views here.
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0 }
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
-    context = {'products':products}
-    return render(request,'store/store.html',context)
+    context = {'products': products, 'cartItems': cartItems}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
 
     if request.user.is_authenticated :
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer,complete = False)
+        order, created = Order.objects.get_or_create(customer=customer,complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}  #for non-logged in users
-    context = {'items':items,'order':order}
+        cartItems = order['get_cart_items']
+
+    context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
     if request.user.is_authenticated :
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer,complete = False)
+        order, created = Order.objects.get_or_create(customer=customer,complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}  #for non-logged in users
-    context = {'items':items,'order':order}
+        cartItems = order['get_cart_items']
+
+
+    context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/checkout.html',context)
 
 def updateItem(request):
